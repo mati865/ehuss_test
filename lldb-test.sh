7d07977456122898b3b33c5f2b697e4bd6d573f2
Eric Huss <eric@huss.org>
2020-11-08 09:41:09 -0800

    Try with build

--- lldb-test.sh
+++ lldb-test.sh
@@ -7,9 +7,18 @@ lldb --version
 
 export PYTHONUNBUFFERED=1
 export PYTHONPATH=$(lldb -P)
-export DYLD_LIBRARY_PATH=`pwd`
+# export DYLD_LIBRARY_PATH=`pwd`
+
+mkdir -p build/test
 
 for i in {1..10}
 do
-    $(xcode-select -p)/usr/bin/python3 lldb_batchmode.py pretty-std-collections pretty-std-collections.debugger.script
+    rustc pretty-std-collections.rs \
+        -C prefer-dynamic \
+        -o build/test/a \
+        -Crpath \
+        -g
+
+    # $(xcode-select -p)/usr/bin/python3
+    /usr/bin/python3 lldb_batchmode.py build/test/a pretty-std-collections.debugger.script
 done
