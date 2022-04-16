46f0b9300200a3ec8663d1441a433c74db299fdf
Eric Huss <eric@huss.org>
2022-04-16 15:40:17 -0700

    tempfile test

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,23 +1,22 @@
-use termcolor::*;
-use std::io::Write;
+use tempfile::TempDir;
+use std::process::Command;
 
-fn main() {
-    let choice = if atty::is(atty::Stream::Stderr) {
-        ColorChoice::Auto
-    } else {
-        ColorChoice::Never
-    };
-    println!("choice={:?}", choice);
-    let mut stderr = StandardStream::stderr(choice);
-    println!("supports={:?}", stderr.supports_color());
-    stderr.reset().unwrap();
-    stderr.set_color(ColorSpec::new().set_bold(true).set_fg(Some(Color::Green))).unwrap();
-    write!(stderr, "Foobar").unwrap();
-    stderr.set_color(ColorSpec::new().set_bold(true)).unwrap();
-    write!(stderr, ":").unwrap();
-    stderr.reset().unwrap();
-    writeln!(stderr, " testing").unwrap();
-
-
+fn output(cmd: &mut Command) {
+    println!("{cmd:?}");
+    let output = cmd.output().unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    if !stdout.is_empty() {
+        println!("{stdout}");
+    }
+    let stderr = String::from_utf8(output.stderr).unwrap();
+    if !stderr.is_empty() {
+        println!("{stderr}");
+    }
 }
 
+fn main() {
+    let td = TempDir::new().unwrap();
+    println!("{:?}", td.path());
+    output(Command::new("ls").arg("-al").arg(td.path()));
+    output(Command::new("who").arg("am").arg("i"));
+}
