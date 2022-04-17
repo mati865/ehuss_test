e8ef00aaade0c37b8498f8a127f1ab37e13e82f0
Eric Huss <eric@huss.org>
2022-04-17 14:11:06 -0700

    Fix local path test

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -160,9 +160,13 @@ fn check(path: &Path) {
 
 fn doit() {
     // let tmp_path = std::env::temp_dir().join(".tmp1234");
-    let tmp_path = PathBuf::from("foo");
+    let tmp_path = PathBuf::from("bar");
+    if tmp_path.exists() {
+        std::fs::remove_dir(&tmp_path).unwrap();
+    }
     std::fs::create_dir(&tmp_path).unwrap();
     // let td = TempDir::new().unwrap();
+    // let tmp_path = td.path();
     println!("{:?}", tmp_path);
     output(Command::new("ls").arg("-al").arg(&tmp_path));
     output(Command::new("who").arg("am").arg("i"));
